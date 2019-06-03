8d35ee0652198d54641fd08b6ac5f59eec6a52f1
SongYao <2817679579@qq.com>
2019-06-04 00:37:50 +0800

    v19.6.3     Lambda表达式与函数式接口

---
+++ src/test/Jam.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Jam {
+
+    public Jam(int s) {
+        System.out.print("传入int参数的构造函数");
+        System.out.println(s);
+    }
+
+    public Jam(String s) {
+        System.out.println("传入String参数的构造函数"+s);
+    }
+}
